4f3c303ceb766398484299d5c00c84d576c952bd
Gowtham <gowthamk91@outlook.com>
2023-02-28 22:24:58 +0530

    Add project files.

---
+++ CSharp11Features/GenericMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp11Features
+{
+    public class GenericMath
+    {
+        public T AddAll<T>(T[] values) where T : INumber<T>
+        {
+            T result = T.Zero;
+
+            foreach (var value in values)
+            {
+                result += value;
+            }
+            return result;
+        }
+    }
+}
---
+++ CSharp11Features/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp11Features;
+
+Console.WriteLine("Hello, World!");
+var genericMath = new GenericMath();
+
+var allNumbers = new[] { 1, 2, 3, 4.5 };
+var result = genericMath.AddAll(allNumbers);
+Console.WriteLine(result);
