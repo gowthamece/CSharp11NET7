82a380cc22d1149297ed5fce2f7ce14669a61cfc
Gowtham <gowthamk91@outlook.com>
2023-03-09 08:28:02 +0530

    Added PatternMatching on span

---
+++ CSharp11Features/PatternMatchingOnSpan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp11Features
+{
+    public class PatternMatchingOnSpan
+    {
+        public void PatternMatchonSpan()
+        {
+            ReadOnlySpan<char> text = "Gowtham K";
+            if (text is "Gowtham K")
+            {
+                Console.WriteLine($"Yes it is {text}");
+            }
+          
+            //Pattern Matching 
+            if(text is ['G',..])
+            {
+                 Console.WriteLine("Name Starts with G");
+            }
+        }
+
+     
+
+    }
+}
--- CSharp11Features/Program.cs
+++ CSharp11Features/Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharp11Features;
 
-Console.WriteLine("Hello, World!");
+//Generic Math
 var genericMath = new GenericMath();
-
 var allNumbers = new[] { 1, 2, 3, 4.5 };
 var result = genericMath.AddAll(allNumbers);
-Console.WriteLine(result);
+Console.WriteLine("----Generic Math----");
+Console.WriteLine($"Sum of the array elements {result}");
+
+//Pattern Matching on Span
+var patternSpan = new PatternMatchingOnSpan();
+patternSpan.PatternMatchonSpan();
